64e6e7cec1b80bd1d7b4bc0d3f0879ff6c19a64d
Annu117 <108427028+Annu117@users.noreply.github.com>
2023-11-18 11:06:41 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import streamlit as st
+import cv2
 import numpy as np
-# import torch
+import torch
+import matplotlib.pyplot as plt
 import copy
-# from utils import model
-# from utils import util
-# from utils.pose import pose_detector
+from utils import model
+from utils import util
+from utils.pose import pose_detector
 import os
-# import pygame
+import pygame
 import time
 from PIL import Image
 
@@ -23,7 +25,7 @@ custom_css = """
                     background-color: #f5f5f5;
                 }
             .main {
-                margin-left: -70px;
+                margin-left: -80px;
                 padding: 20px;
                 margin-top:  -110px; 
             }
@@ -93,11 +95,9 @@ def general_guidline():
         st.write("")
         B_posture_image = Image.open("W_sitting_pose.png")
         st.image(B_posture_image, caption="Bad Sitting Posture", use_column_width=True)
-    # Load and display the image
-    # posture_image = Image.open("Ergo_Check1.png")
-    # st.image(posture_image, caption="Sitting Posture", use_column_width=True)
     st.markdown("---")
-    st.subheader("Position for Safety and Comfort")
+    st.markdown("<div class='section-title'>Position for Safety and Comfort</div>", unsafe_allow_html=True)
+
     guidelines = [
         "HEAD level in line with the torso",
         "FOREARMS, WRISTS, and HANDS relaxed and straight",
@@ -111,7 +111,7 @@ def general_guidline():
     for guideline in guidelines:
         st.markdown(f" - {guideline}")
 
-    st.subheader("Remember")
+    st.markdown("<div class='section-title'>Remember</div>", unsafe_allow_html=True)
     remember_points = [
         "WHEN KEYING, float your hands over the keyboard, and use palm rests between bursts of keying.",
         "WHEN MOUSING, keep your wrists straight, and use the elbow to pivot.",
@@ -196,183 +196,183 @@ def yoga_sequence():
 
     )
 
-# def RealTimeMonitor():
-#     st.markdown('<link rel="stylesheet" type="text/css" href="styles.css">', unsafe_allow_html=True)
+def RealTimeMonitor():
+    st.markdown('<link rel="stylesheet" type="text/css" href="styles.css">', unsafe_allow_html=True)
 
-#     start_time = 0
-#     end_time = 0
-#     # Initialize the model and pose detector
-#     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-#     model_rula = model.NN_base().to(device)
-#     model_rula.load_state_dict(torch.load(os.path.join('.', 'weights', 'rula.pth'), map_location=torch.device('cpu')))
-#     pose_estimation = pose_detector(os.path.join(".", "weights", "pose_coco.pth"))
+    start_time = 0
+    end_time = 0
+    # Initialize the model and pose detector
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model_rula = model.NN_base().to(device)
+    model_rula.load_state_dict(torch.load(os.path.join('.', 'weights', 'rula.pth'), map_location=torch.device('cpu')))
+    pose_estimation = pose_detector(os.path.join(".", "weights", "pose_coco.pth"))
 
-#     pygame.mixer.init()
-#     alert_sound = pygame.mixer.Sound("audio_file.mp3")
-#     st.markdown("<div class='section-title'>Real-time Posture risk analysis</div>", unsafe_allow_html=True)
+    pygame.mixer.init()
+    alert_sound = pygame.mixer.Sound("audio_file.mp3")
+    st.markdown("<div class='section-title'>Real-time Posture risk analysis</div>", unsafe_allow_html=True)
 
-#     col1, col2 = st.columns(2)
-#     with col1:
-#         col3, col4 = st.columns(2)
-#         with col3:
-#             start_button = st.button("Start Analysis")
-#         with col4:
-#             stop_button = st.button("Stop Analysis")  
+    col1, col2 = st.columns(2)
+    with col1:
+        col3, col4 = st.columns(2)
+        with col3:
+            start_button = st.button("Start Analysis")
+        with col4:
+            stop_button = st.button("Stop Analysis")  
 
-#     with col2:
-#         video_capture = cv2.VideoCapture(0)
-#         video_element = st.empty()
+    with col2:
+        video_capture = cv2.VideoCapture(0)
+        video_element = st.empty()
 
-#     # Analysis loop
-#     analysis_running = False
-#     analysis_started = False
-#     scores = []
-#     level_counts = {"Level 1": 0, "Level 2": 0, "Level 3": 0, "Level 4": 0}
+    # Analysis loop
+    analysis_running = False
+    analysis_started = False
+    scores = []
+    level_counts = {"Level 1": 0, "Level 2": 0, "Level 3": 0, "Level 4": 0}
 
-#     while video_capture.isOpened():
-#         if start_button and not stop_button:
-#             analysis_running = True
-#             analysis_started = True
-#             scores = []
-#             alert_playing = False
-#             start_time = time.time()
+    while video_capture.isOpened():
+        if start_button and not stop_button:
+            analysis_running = True
+            analysis_started = True
+            scores = []
+            alert_playing = False
+            start_time = time.time()
             
-#         if analysis_running and not stop_button:
-#             _, frame = video_capture.read()
-#             image_width = 640  
-#             image_height = int(image_width * 3 / 4)
+        if analysis_running and not stop_button:
+            _, frame = video_capture.read()
+            image_width = 640  
+            image_height = int(image_width * 3 / 4)
 
-#             frame = cv2.resize(frame, (image_width, image_height))
+            frame = cv2.resize(frame, (image_width, image_height))
 
-#             # Pose detection
-#             candidate, subset = pose_estimation(frame)
+            # Pose detection
+            candidate, subset = pose_estimation(frame)
 
-#             if len(subset) < 1:
-#                 st.warning("No poses detected in the webcam feed.")
-#             else:
-#                 al_list = []
-#                 for i in range(len(subset)):
-#                     if subset[i][8] == -1 or subset[i][11] == -1:
-#                         # # st.warning(f"The {i + 1}th person has missing points")
-#                         break
-#                     mid_x1 = candidate[subset[i][8].astype(int)][0]
-#                     mid_x2 = candidate[subset[i][11].astype(int)][0]
-#                     mid_y1 = candidate[subset[i][8].astype(int)][1]
-#                     mid_y2 = candidate[subset[i][11].astype(int)][1]
-#                     mid_x = (mid_x1 + mid_x2) / 2
-#                     mid_y = (mid_y1 + mid_y2) / 2
+            if len(subset) < 1:
+                st.warning("No poses detected in the webcam feed.")
+            else:
+                al_list = []
+                for i in range(len(subset)):
+                    if subset[i][8] == -1 or subset[i][11] == -1:
+                        # # st.warning(f"The {i + 1}th person has missing points")
+                        break
+                    mid_x1 = candidate[subset[i][8].astype(int)][0]
+                    mid_x2 = candidate[subset[i][11].astype(int)][0]
+                    mid_y1 = candidate[subset[i][8].astype(int)][1]
+                    mid_y2 = candidate[subset[i][11].astype(int)][1]
+                    mid_x = (mid_x1 + mid_x2) / 2
+                    mid_y = (mid_y1 + mid_y2) / 2
 
-#                     pose_xy = -1 * np.ones((18, 2))
-#                     for jj in range(18):
-#                         if subset[i][jj] > -1:
-#                             pose_xy[jj][:] = candidate[subset[i][jj].astype(int)][:2]
-#                             pose_xy[jj][0] -= mid_x
-#                             pose_xy[jj][1] -= mid_y
-#                     pose_xy = pose_xy.reshape((1, 36))
-#                     y_pred = model_rula(torch.tensor(pose_xy).float().to(device))
+                    pose_xy = -1 * np.ones((18, 2))
+                    for jj in range(18):
+                        if subset[i][jj] > -1:
+                            pose_xy[jj][:] = candidate[subset[i][jj].astype(int)][:2]
+                            pose_xy[jj][0] -= mid_x
+                            pose_xy[jj][1] -= mid_y
+                    pose_xy = pose_xy.reshape((1, 36))
+                    y_pred = model_rula(torch.tensor(pose_xy).float().to(device))
 
-#                     # Predicted action level
-#                     y_pred = torch.argmax(y_pred)
-#                     al_list.append(y_pred.item())
+                    # Predicted action level
+                    y_pred = torch.argmax(y_pred)
+                    al_list.append(y_pred.item())
 
-#                 # Draw annotations directly on the frame
-#                 for i in range(len(subset)):
-#                     if i < len(al_list):
-#                         if al_list:
-#                             cv2.putText(frame, f"Person {i + 1}: Level {al_list[i] + 1}", (10, 30 + 30 * i), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 1255), 2)
-#                             print(al_list[i] + 1)
-#                             if al_list[i] + 1 == 1:
-#                                 level_counts["Level 1"] += 1
-#                             elif al_list[i] + 1 == 2:
-#                                 level_counts["Level 2"] += 1
-#                                 print("Level 2: " + str(level_counts["Level 2"]))
-#                             elif al_list[i] + 1 == 3:
-#                                 level_counts["Level 3"] += 1
-#                                 print("Level 3: " + str(level_counts["Level 3"]))
+                # Draw annotations directly on the frame
+                for i in range(len(subset)):
+                    if i < len(al_list):
+                        if al_list:
+                            cv2.putText(frame, f"Person {i + 1}: Level {al_list[i] + 1}", (10, 30 + 30 * i), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 1255), 2)
+                            print(al_list[i] + 1)
+                            if al_list[i] + 1 == 1:
+                                level_counts["Level 1"] += 1
+                            elif al_list[i] + 1 == 2:
+                                level_counts["Level 2"] += 1
+                                print("Level 2: " + str(level_counts["Level 2"]))
+                            elif al_list[i] + 1 == 3:
+                                level_counts["Level 3"] += 1
+                                print("Level 3: " + str(level_counts["Level 3"]))
 
-#                             elif al_list[i] + 1 == 4:
-#                                 level_counts["Level 4"] += 1
-#                             # level_counts[f"Level {al_list[i] + 1}"] += 1
-#                             if al_list[i] + 1 > 2 and not alert_playing:
-#                                 alert_sound.play()
-#                                 alert_playing = True
+                            elif al_list[i] + 1 == 4:
+                                level_counts["Level 4"] += 1
+                            # level_counts[f"Level {al_list[i] + 1}"] += 1
+                            if al_list[i] + 1 > 2 and not alert_playing:
+                                alert_sound.play()
+                                alert_playing = True
 
-#                             elif al_list[i] + 1 <= 2 and alert_playing:
-#                                 pygame.mixer.stop()
-#                                 alert_playing = False
+                            elif al_list[i] + 1 <= 2 and alert_playing:
+                                pygame.mixer.stop()
+                                alert_playing = False
 
-#             # Display the resized frame with annotations and clear the previous image
-#             video_element.image(frame, channels="BGR", use_column_width=True, output_format="BGR")
+            # Display the resized frame with annotations and clear the previous image
+            video_element.image(frame, channels="BGR", use_column_width=True, output_format="BGR")
 
-#         if stop_button :
-#             analysis_running = False
-#             alert_playing = False
-#             end_time = time.time()
-#             st.write(scores)
+        if stop_button :
+            analysis_running = False
+            alert_playing = False
+            end_time = time.time()
+            st.write(scores)
             
-#             print(level_counts)
-#             for key, value in level_counts.items():
-#                 st.write(f'{key}: {value}')
+            print(level_counts)
+            for key, value in level_counts.items():
+                st.write(f'{key}: {value}')
 
-#             break
+            break
 
-#     video_capture.release()
-#     pygame.quit()
-#     total_time = end_time - start_time
+    video_capture.release()
+    pygame.quit()
+    total_time = end_time - start_time
 
-# def ImageAnalysis():
-#     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-#     model_rula = model.NN_base().to(device)
-#     model_rula.load_state_dict(torch.load(os.path.join('.', 'weights', 'rula.pth'), map_location=torch.device('cpu')))
-#     pose_estimation = pose_detector(os.path.join(".", "weights", "pose_coco.pth"))
+def ImageAnalysis():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model_rula = model.NN_base().to(device)
+    model_rula.load_state_dict(torch.load(os.path.join('.', 'weights', 'rula.pth'), map_location=torch.device('cpu')))
+    pose_estimation = pose_detector(os.path.join(".", "weights", "pose_coco.pth"))
 
-#     st.subheader("Image analysis based on rula score")
-#     uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+    st.subheader("Image analysis based on rula score")
+    uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
-#     if uploaded_image is not None:
-#         col1, col2 = st.columns(2)
-#         # with col1:
-#         img = cv2.imdecode(np.fromstring(uploaded_image.read(), np.uint8), 1)
+    if uploaded_image is not None:
+        col1, col2 = st.columns(2)
+        # with col1:
+        img = cv2.imdecode(np.fromstring(uploaded_image.read(), np.uint8), 1)
 
-#         # Pose detection
-#         candidate, subset = pose_estimation(img)
+        # Pose detection
+        candidate, subset = pose_estimation(img)
 
-#         if len(subset) < 1:
-#             st.write("No poses detected in the uploaded image.")
-#         else:
-#             al_list = []
-#             with col2:
-#                 st.write("**Risk Level**")
-#             for i in range(len(subset)):
-#                 if subset[i][8] == -1 or subset[i][11] == -1:
-#                     st.write(f"The {i + 1}th person has missing points")
-#                     break
-#                 mid_x1 = candidate[subset[i][8].astype(int)][0]
-#                 mid_x2 = candidate[subset[i][11].astype(int)][0]
-#                 mid_y1 = candidate[subset[i][8].astype(int)][1]
-#                 mid_y2 = candidate[subset[i][11].astype(int)][1]
-#                 mid_x = (mid_x1 + mid_x2) / 2
-#                 mid_y = (mid_y1 + mid_y2) / 2
+        if len(subset) < 1:
+            st.write("No poses detected in the uploaded image.")
+        else:
+            al_list = []
+            with col2:
+                st.write("**Risk Level**")
+            for i in range(len(subset)):
+                if subset[i][8] == -1 or subset[i][11] == -1:
+                    st.write(f"The {i + 1}th person has missing points")
+                    break
+                mid_x1 = candidate[subset[i][8].astype(int)][0]
+                mid_x2 = candidate[subset[i][11].astype(int)][0]
+                mid_y1 = candidate[subset[i][8].astype(int)][1]
+                mid_y2 = candidate[subset[i][11].astype(int)][1]
+                mid_x = (mid_x1 + mid_x2) / 2
+                mid_y = (mid_y1 + mid_y2) / 2
 
-#                 pose_xy = -1 * np.ones((18, 2))
-#                 for jj in range(18):
-#                     if subset[i][jj] > -1:
-#                         pose_xy[jj][:] = candidate[subset[i][jj].astype(int)][:2]
-#                         pose_xy[jj][0] -= mid_x
-#                         pose_xy[jj][1] -= mid_y
-#                 pose_xy = pose_xy.reshape((1, 36))
-#                 y_pred = model_rula(torch.tensor(pose_xy).float().to(device))
+                pose_xy = -1 * np.ones((18, 2))
+                for jj in range(18):
+                    if subset[i][jj] > -1:
+                        pose_xy[jj][:] = candidate[subset[i][jj].astype(int)][:2]
+                        pose_xy[jj][0] -= mid_x
+                        pose_xy[jj][1] -= mid_y
+                pose_xy = pose_xy.reshape((1, 36))
+                y_pred = model_rula(torch.tensor(pose_xy).float().to(device))
 
-#             # Predicted action level
-#                 y_pred = torch.argmax(y_pred)
-#                 al_list.append(y_pred.item())
-#                 with col2:
+            # Predicted action level
+                y_pred = torch.argmax(y_pred)
+                al_list.append(y_pred.item())
+                with col2:
 
-#                     st.write(f" {i + 1}th person: {y_pred.item() + 1}")
-#             with col1:
-#                 # Display the image with results
-#                 img = util.pose_vis(img, candidate, subset, al_list)
-#                 st.image(img, channels="BGR")
+                    st.write(f" {i + 1}th person: {y_pred.item() + 1}")
+            with col1:
+                # Display the image with results
+                img = util.pose_vis(img, candidate, subset, al_list)
+                st.image(img, channels="BGR")
 
     
 
@@ -384,8 +384,8 @@ with General_Guidlines:
     general_guidline()
 # with Real_Time_Monitor:
 #     RealTimeMonitor()
-# with Image_Analysis:
-#     ImageAnalysis()
+with Image_Analysis:
+    ImageAnalysis()
 with Report:
     report()
 with Yoga_Poses:
