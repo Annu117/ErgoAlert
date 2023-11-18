07d3288280e7a70e596fec3313431ce574c14392
Annu117 <108427028+Annu117@users.noreply.github.com>
2023-11-18 10:26:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import streamlit as st
 import numpy as np
 # import torch
-import matplotlib.pyplot as plt
 import copy
-from utils import model
-from utils import util
-from utils.pose import pose_detector
+# from utils import model
+# from utils import util
+# from utils.pose import pose_detector
 import os
-import pygame
+# import pygame
 import time
 from PIL import Image
 
